f3e94065e3505e84bb6829f12cb1e781a1023fd1
Anastecia6650 <benbella6650@gmail.com>
2022-04-10 05:40:25 -0700

    Task4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -31,3 +31,12 @@ putchar(10);
 
 	return (0);
 }
+  #include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet in lower then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
