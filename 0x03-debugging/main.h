e736b8e6dd7a534f41166a90354204be0274eefd
Anastecia6650 <benbella6650@gmail.com>
2022-04-14 16:12:42 -0700

    Task3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -36,3 +36,13 @@ int largest_number(int a, int b, int c)
 
     return (largest);
 }
+cat main.h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
