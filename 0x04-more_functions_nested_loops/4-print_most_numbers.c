a3574b009dea057e8ba46e79ce175b126e3f7591
Anastecia6650 <benbella6650@gmail.com>
2022-04-14 17:02:57 -0700

    Task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int n = 0;
+int n = 0;
 
-	while (n < 10)
-	{
-		if (n != 2 && n != 4)
-			_putchar(n + '0');
+while (n < 10)
+{
+if (n != 2 && n != 4)
+_putchar(n + '0');
 n += 1;
-		    n += 1;
-	}
-	_putchar('\n');
+}
+_putchar('\n');
 }
