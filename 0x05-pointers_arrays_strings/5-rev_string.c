b8c4da9acf6a773fe2d4dc4024950bfd9fdeb4a9
Anastecia6650 <benbella6650@gmail.com>
2022-04-20 05:23:09 -0700

    Task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,20 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
-	int aux = 0;
-	char ltemp;
+int i = 0;
+int aux = 0;
+char ltemp;
 
-	while (*(s + i) != '\0')
-		i += 1;
-	i -= 1;
+while (*(s + i) != '\0')
+i += 1;
+i -= 1;
 
 while (aux < i)
-	{
-		ltemp = s[i];
-		s[i] = s[aux];
-		s[aux] = ltemp;
-		aux++;
-		i--;
-	}
+{
+ltemp = s[i];
+s[i] = s[aux];
+s[aux] = ltemp;
+aux++;
+i--;
+}
 }
