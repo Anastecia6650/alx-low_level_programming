e80afb451e1e1dd70163580fcc8adaad89f6f856
Anastecia6650 <benbella6650@gmail.com>
2022-04-23 12:24:40 -0700

    Task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,16 @@
  * Return: pointer to resulting `dest`
  */
 
-char *_strncat(char *dest, char *src, int n)
+ char *_strncat(char *dest, char *src, int n)
 {
-	int i, c;
+int i, c;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+for (i = 0; dest[i] != '\0'; i++);
+		
 
 for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
-	{
-		dest[i] = src[c];
-	}
-	return (dest);
+{
+dest[i] = src[c];
+}
+return (dest);
 }
