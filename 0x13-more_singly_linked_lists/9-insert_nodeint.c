3d46b48c023dfdaa8ff00b0e8b87a5885d849b4f
Anastecia6650 <benbella6650@gmail.com>
2022-05-26 15:14:06 -0700

    Task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *h;
 
+	h = *head;
+
 	if (idx != 0)
 	{
 		for (i = 0; i < idx - 1 && h != NULL; i++)
